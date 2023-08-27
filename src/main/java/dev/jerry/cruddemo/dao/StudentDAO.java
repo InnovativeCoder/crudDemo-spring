6ff7c7bca31c14f88ee6bd19c40b0b7cdbfc6b83
InnovativeCoder <aashitsingh2010@gmail.com>
2023-08-27 00:22:55 -0400

    Find All method in db

--- src/main/java/dev/jerry/cruddemo/dao/StudentDAO.java
+++ src/main/java/dev/jerry/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,12 @@ package dev.jerry.cruddemo.dao;
 
 import dev.jerry.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
